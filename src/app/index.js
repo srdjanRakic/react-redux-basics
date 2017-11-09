ced9ed3b53d0bf76df1c09370bf44bfe58fbb262
srdjanRakic <srg_ian@hotmail.com>
2017-11-09 10:30:11 +0100

    Implement Redux store

--- src/app/index.js
+++ src/app/index.js
@@ -1,31 +1,70 @@
 import React from "react";
 import {render} from "react-dom";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import App from './components/App';
+import { Provider } from "react-redux"
 
-import { User } from './components/User';
-import { Main } from './components/Main';
-
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            username: "Max"
-        };
+const mathReducer = (state = {
+    result: 1,
+    lastValues: []
+}, action) => {
+    switch (action.type) {
+        case "ADD":
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
+            break;
+        case "SUBTRACT":
+            state = {
+                ...state,
+                result: state.result - action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
+            break;
     }
+    return state;
+};
 
-    changeUsername(newName) {
-        this.setState({
-            username: newName
-        });
+const userReducer = (state = {
+    name: "Srdjan",
+    age: 28
+}, action) => {
+    switch (action.type) {
+        case "SET_NAME":
+            state = {
+                ...state,
+                name: action.payload
+            };
+            break;
+        case "SET_AGE":
+            state = {
+                ...state,
+                age: action.payload
+            };
+            break;
     }
+    return state;
+};
 
-    render() {
-        return (
-            <div className="container">
-                <Main changeUsername={this.changeUsername.bind(this)}/>
-                <User username={this.state.username}/>
-            </div>
-        );
-    }
+const myLogger = (store) => (next) => (action) => {
+    console.log("Logged Action ", action);
+    next(action);
 }
 
-render(<App />, window.document.getElementById('app'));
+const store = createStore(
+            combineReducers({mathReducer, userReducer}),
+            {}, 
+            applyMiddleware(createLogger())
+);
+
+store.subscribe(() => {
+    //console.log("Store updated!", store.getState());
+});
+
+render(<Provider store={store}>
+            <App />
+        </Provider>, 
+         window.document.getElementById('app'));
