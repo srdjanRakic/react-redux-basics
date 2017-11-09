d56f81e984a685de9f0398c09d1af67ba9a85415
srdjanRakic <srg_ian@hotmail.com>
2017-11-09 10:34:58 +0100

    Seperate components and containers

---
+++ src/app/containers/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {connect} from "react-redux";
+import { User } from '../components/User';
+import { Main } from '../components/Main';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <Main changeUsername={() => this.props.setName("Srle")}/>
+                <User username={this.props.user.name}/>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.userReducer,
+        math: state.mathReducer
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setName: (name) => {
+            dispatch({
+                type: "SET_NAME",
+                payload: name
+            });
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
